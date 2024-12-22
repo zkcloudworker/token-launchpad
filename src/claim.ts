eb7dd7b05b6c76a9fb008bdb7d313a77c0bd5fcb
DFST <mk@dfst.io>
2024-12-22 15:47:09 +0300

    move compile script to library

--- src/claim.ts
+++
@@ -1,1 +0,0 @@
-//export class FungibleTokenClaim extends SmartContract({}) {}
--- test/compile-mainnet.test.ts
+++
@@ -1,6 +0,0 @@
-import { describe, it } from "node:test";
-import { compileContracts } from "./helpers/compile.js";
-
-describe("Contracts verification keys on mainnet", async () => {
-  await compileContracts("mainnet");
-});
